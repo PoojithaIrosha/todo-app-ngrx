d1f240c37898d236c987dbe8d9a7766ca9859278
Poojitha Irosha <poojithairosha9311@gmail.com>
2025-09-07 19:33:06 +0530

    clean up project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,36 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, isDevMode, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideStore} from '@ngrx/store';
+import {todosFeatureKey, todosReducer} from './features/todo/todos.reducer';
+import {storageSyncMetaReducer} from './core/meta-reducers/storage-sync.metareducer';
+import {provideEffects} from '@ngrx/effects';
+import {provideStoreDevtools} from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideStore(
+      { [todosFeatureKey]: todosReducer },
+      {
+        metaReducers: [storageSyncMetaReducer as any],
+        runtimeChecks: {
+          strictStateImmutability: true,
+          strictActionImmutability: true,
+          strictStateSerializability: true,
+          strictActionSerializability: true,
+          strictActionWithinNgZone: true,
+          strictActionTypeUniqueness: true,
+        },
+      }
+    ),
+    provideEffects(),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      connectInZone: true,
+    }),
+  ],
 };
